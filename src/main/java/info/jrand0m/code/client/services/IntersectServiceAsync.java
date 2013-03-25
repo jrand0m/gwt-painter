c36d525ffbd3676b434748812f9d3588ce16a6d7
Mike Stetsyshyn <jays.demons@gmail.com>
2013-03-25 10:49:25 +0200

    COnflitcts with autogenerated

--- src/main/java/info/jrand0m/code/client/services/IntersectServiceAsync.java
+++
@@ -1,8 +0,0 @@
-package info.jrand0m.code.client.services;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface IntersectServiceAsync {
-    // Sample interface method of remote interface
-    void getIntersection(String figure1, String figure2, AsyncCallback<String> async);
-}
